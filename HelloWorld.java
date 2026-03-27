72a2c676faa308e8cc187734bcde306263fdbc2b
Ecren Donmez <ecren@cncsistem.com>
2026-03-27 12:17:06 +0000

    Change sayMessage in main branch

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
+    public static void sayMessage() {
+        System.out.println("Hello from main branch!");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        sayMessage();
     }
 }
